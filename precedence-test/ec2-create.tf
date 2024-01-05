5fd38cfa0a874af4826d5983950716b287b05440
suresh <mailsuresh@gmail.com>
2024-01-05 21:01:37 +0530

    added priority precedence in comments

--- precedence-test/ec2-create.tf
+++ precedence-test/ec2-create.tf
@@ -1,3 +1,8 @@
+#1.command line variable (terraform plan -var="instance-type=t2.large")
+#2.command line file (terraform plan -var-file="vars.tfvars")
+#3.terraform.tfvars
+#4.variable.tf
+#check the priority of environmental variable by testing
 resource "aws_instance" "myec2" {
   ami           = var.ami-id
   instance_type = var.instance-type
